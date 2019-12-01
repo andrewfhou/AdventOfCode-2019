cbee363f9c1ed70054a2fb9a849d4a990f899551
Andrew Hou <andrewfhou@gmail.com>
2019-11-30 21:21:12 -0500

    init dayone

---
+++ dayOne/dayOne.py
@@ -0,0 +1,18 @@
+import time
+
+curr_ms = lambda: time.time() * 1000
+
+with open("input.txt") as file:
+    inputs = file.read().strip().split()
+
+def partOne():
+
+def partTwo():
+
+def main():
+    print("AoC Day One")
+    partOne()
+    print("Part One - time taken: " + str(curr_ms() - start) + "ms\n")
+    partTwo()
+    print("Part Two - time taken: " + str(curr_ms() - start) + "ms\n")
+
