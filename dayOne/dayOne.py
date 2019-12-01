57ea238c67c86e89a13858268bdbce4b2eab8164
Andrew Hou <andrewfhou@gmail.com>
2019-12-01 01:09:46 -0500

    day one solved

--- dayOne/dayOne.py
+++ dayOne/dayOne.py
@@ -1,18 +1,37 @@
 import time
 
-curr_ms = lambda: time.time() * 1000
+CURR_MS = lambda: time.time() * 1000
 
 with open("input.txt") as file:
     inputs = file.read().strip().split()
 
 def partOne():
+    sum = 0
+    for x in inputs:
+        sum += int(int(x) / 3) - 2
+    return sum
 
 def partTwo():
+    sum = 0
+    for x in inputs:
+        sum += computeFuel(int(x))
+    return sum
 
-def main():
-    print("AoC Day One")
-    partOne()
-    print("Part One - time taken: " + str(curr_ms() - start) + "ms\n")
-    partTwo()
-    print("Part Two - time taken: " + str(curr_ms() - start) + "ms\n")
+def computeFuel(fuel):
+    if fuel <= 0:
+        return 0
+    else:
+        needed = (int(int(fuel) / 3) - 2)
+        needed = 0 if needed < 0 else needed
+        return needed + computeFuel(needed)
+
+print("[ADVENT OF CODE - DAY01]\n")
+start = CURR_MS()
+
+print("PART ONE - " + str(partOne()))
+print("PART ONE TIME - %.6fms\n" % (CURR_MS() - start))
+
+twoStart = CURR_MS()
+print("PART TWO - " + str(partTwo()))
+print("PART TWO TIME - %.6fms" % (CURR_MS() - twoStart))
 
