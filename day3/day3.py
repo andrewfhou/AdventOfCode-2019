7ebf0fe715f038c90dbeff775b9d438ec1c028d0
Andrew Hou <andrewfhou@gmail.com>
2019-12-03 13:44:18 -0500

    some timing stuff for day3

--- day3/day3.py
+++ day3/day3.py
@@ -19,41 +19,43 @@ def trace_wire(wire):
     for mov in wire:
         direction = mov[0]
         mag = int(mov[1:])
-    
+
         for _ in range(mag):
             x = x + mov_x[direction]
             y = y + mov_y[direction]
             steps = steps + 1
-            
+
             if (x,y) not in pts:
                 pts[(x,y)] = steps
     return pts
 
-
-points_a = trace_wire(wire_a)
-points_b = trace_wire(wire_b)
-
-def partOne():
+def partOne(points_a, points_b):
     intersections = points_a.keys() & points_b.keys()
     minDist = float('inf')
     for (x,y) in intersections:
         minDist = min(minDist, abs(x) + abs(y))
     return minDist
 
-def partTwo():
+def partTwo(points_a, points_b):
     intersections = points_a.keys() & points_b.keys()
     minSteps = float('inf')
     for x in intersections:
         minSteps = min(minSteps, points_a[x] + points_b[x])
     return minSteps
 
+ONE_START = CURR_MS()
+
 print("[ADVENT OF CODE - DAY03]\n")
-start = CURR_MS()
 
-print("PART ONE - " + str(partOne()))
-print("PART ONE TIME - %.6fms\n" % (CURR_MS() - start))
+pts_a = trace_wire(wire_a)
+pts_b = trace_wire(wire_b)
+print("PART ONE - " + str(partOne(pts_a, pts_b)))
+print("PART ONE TIME - %.6fms\n" % (CURR_MS() - ONE_START))
 
-twoStart = CURR_MS()
-print("PART TWO - " + str(partTwo()))
-print("PART TWO TIME - %.6fms" % (CURR_MS() - twoStart))
+TWO_START = CURR_MS()
+
+pts_a = trace_wire(wire_a)
+pts_b = trace_wire(wire_b)
+print("PART TWO - " + str(partTwo(pts_a, pts_b)))
+print("PART TWO TIME - %.6fms" % (CURR_MS() - TWO_START))
 
