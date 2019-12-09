9d013f143b962348e5e6888558c189d5f97f75fc
Andrew Hou <andrewfhou@gmail.com>
2019-12-09 12:49:17 -0500

    INTCODE COMPUTER COMPLETE

--- intcode.py
+++ intcode.py
@@ -3,18 +3,38 @@ from collections import defaultdict
 with open("intcode_test.txt") as file:
     inputs = file.read().strip().split(',')
 
-def run_intcode(code, prog_inputs):
-    # OPCODES                                            1  2  3  4  5  6  7  8
-    arg_sizes = defaultdict(lambda:0, dict(enumerate([0, 3, 3, 1, 1, 2, 2, 3, 3])))
+def run_intcode(prog_inputs):
+    # OPCODES                                            1  2  3  4  5  6  7  8  9
+    arg_sizes = defaultdict(lambda:0, dict(enumerate([0, 3, 3, 1, 1, 2, 2, 3, 3, 1])))
 
-    def get_arg(arg, instr_pt, write):
-        return code[instr_pt+arg] if write \
-            else (code[code[instr_pt+arg]] if int(instr[-(2+arg)]) == 0 else code[instr_pt+arg])
-
-    code = list(map(int, code))
+    code = list(map(int, inputs))
     prog_inputs = list(prog_inputs)
     outputs = []
     instr_pt = 0
+    rel_base = 0
+
+    def get_arg(arg_idx, instr_pt, write):
+        arg_param = int(instr[-(2+arg_idx)])
+
+        if write:
+            wrt_idx = code[instr_pt + arg_idx]
+            if wrt_idx + rel_base >= len(code): # expand allocated mem if needed
+                code.extend([0] * int((wrt_idx - len(code) + rel_base) * 1.5))
+            if arg_param == 2:
+                wrt_idx = wrt_idx + rel_base
+            return wrt_idx
+        elif arg_param == 0:
+            pos = code[instr_pt+arg_idx]
+            try: return code[pos]
+            except IndexError: return 0 # out of existing mem, return default
+        elif arg_param == 1:
+            immediate = instr_pt + arg_idx
+            try: return code[immediate]
+            except IndexError: return 0 # out of existing mem, return default
+        elif arg_param == 2:
+            rel_pos = rel_base + code[instr_pt+arg_idx]
+            try: return code[rel_pos]
+            except IndexError: return 0 # out of existing mem, return default
 
     while True:
         jump =  False
@@ -63,6 +83,9 @@ def run_intcode(code, prog_inputs):
             arg2 = get_arg(2, instr_pt, False)
             arg3 = get_arg(3, instr_pt, True)
             code[arg3] = 1 if arg1 == arg2 else 0
+        elif opcode == 9: # ADJUST RELATIVE BASE OFFSET
+            arg1 = get_arg(1, instr_pt, False)
+            rel_base = rel_base + arg1
         if not jump: instr_pt = instr_pt + (argsize + 1)
 
 print(run_intcode(inputs, [5]))
